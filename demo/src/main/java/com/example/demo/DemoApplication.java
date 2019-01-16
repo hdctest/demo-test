fd03e771659428ed79bd42b753fe608363b62931
user3 <user3@192.168.0.153>
2019-01-16 16:16:36 +0800

    123

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -8,7 +8,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
 
     public static void main(String[] args) {
-    	//123test
         SpringApplication.run(DemoApplication.class, args);
     }
 
